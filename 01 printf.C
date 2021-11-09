2bc1bd12ef8760dca51a382e015f10923195cbbf
Anand47007 <93970861+Anand47007@users.noreply.github.com>
2021-11-09 19:14:29 +0530

    Add files via upload

---
+++ 01 printf.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No=0;
+
+    printf("\nValue of Failure is %d",No);
+
+    No=45;
+
+    printf("\nValue of Success is %d",No);
+
+    getch();
+    return 0;
+
+}
