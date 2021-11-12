6431895434acec8c033c2681f6f779b7baad7aa3
Anand47007 <93970861+Anand47007@users.noreply.github.com>
2021-11-13 04:31:43 +0530

    Add files via upload

---
+++ ASCII/1.To Convert Character into its ASCII value.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+        char ch=0;
+
+        printf("\n Character to ASCII conversion =>\n\n");
+
+        printf("\n Enter Character to get its corresponding ASCII value = ");
+        scanf("%c",&ch);
+
+        printf("\n\n ASCII value for given Character %c is = |%d| ",ch,ch);
+        printf("\n\n Thanks for using our service");
+
+        getch();
+        return 0;
+}
---
+++ ASCII/Program To Accept ASCII Value & Display Its Corresponding Character.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+        int No=0;
+
+        printf("\n\n ASCII to Character Conversion =>\n\n");
+
+        printf("\n Enter ASCII Value to Find Its Corresponding Character = ");
+        scanf("%d",&No);
+
+        printf("\n\n Character Value of Given ASCII Value %d is |%c|",No,No);
+
+        printf("\n\n Thanks For Using Our Services");
+
+        getch();
+        return 0;
+}
